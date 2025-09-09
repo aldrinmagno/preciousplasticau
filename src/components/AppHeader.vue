75b5d37d283fee870d4a38dfdd370b59270d8cff
Buzz <aldrinmagno@gmail.com>
2025-09-10 01:43:29 +0930

    Remove Home from navigation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,6 @@
         Precious Plastic Darwin
       </div>
       <ul class="nav-links" :style="{ display: showMobileMenu ? 'flex' : '' }">
-        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#features">What We Do</a></li>
         <li><a href="#newsletter">Newsletter</a></li>
